2508dcb7d195ef86e0f8ef743cdcd3cbadd94bee
nadheesh <nadheeshj@gmail.com>
2019-11-21 09:01:19 +0530

    improve deploy app script

--- deploy_app.sh
+++ deploy_app.sh
@@ -1,32 +1,48 @@
 #!/usr/bin/env bash
 
-cd sample_apps
 
 JAR_NAME="sample_apps-0.1.0.jar"
 IMAGE_NAME="sample_app"
 
-ECHO_IMAGE="echo"
-PRIME_IMAGE="prime"
+mkdir stat
+stat_dir=stat/$(date +%Y%m%d_%H%M%S)
+mkdir ${stat_dir}
+
+echo saving stats at ${stat_dir}
+
+volume_path=$(realpath ${stat_dir})
+
+export volume_path=${volume_path} && \
+export work_path=/usr/app/temp && \
+
+echo
+cd sample_apps
+
+
+echo stoping all the containers
+docker stop $(docker ps -a -q)
 
 echo building the jar
 mvn clean install
 
 echo Removing existing images and containers
 
-docker rmi -f $IMAGE_NAME
+docker rmi -f ${IMAGE_NAME}
+
+echo build the docker images ${IMAGE_NAME}
+docker build ./ -t ${IMAGE_NAME}
+
 docker system prune -f
 
-
-echo build the docker images $IMAGE_NAME
-docker build ./ -t $IMAGE_NAME
-
-echo deploy the docker image $IMAGE_NAME
+echo deploy the docker image ${IMAGE_NAME}
 
 if [[ $# < 2 ]]
 then
-    docker run -p 8090:8090 --cpus 2 --name $IMAGE_NAME $IMAGE_NAME
+    docker run --volume "${volume_path}":${work_path} \
+        -p 8090:8090 -d --cpus 2 --name ${IMAGE_NAME} ${IMAGE_NAME}
 else
-    docker run -p 8090:8090 --cpus 2 --name $IMAGE_NAME --link $1 --link $2 $IMAGE_NAME
+    docker run --volume "${volume_path}":${work_path} \
+        -p 8090:8090 -d --cpus 2 --name ${IMAGE_NAME} --link $1 --link $2 ${IMAGE_NAME}
 fi
 
 
