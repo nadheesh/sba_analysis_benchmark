bc6a75a54f684b3261176dbf5f62e4deaf3e6b02
nadheesh <nadheeshj@gmail.com>
2020-01-31 09:06:49 +0530

    add all app calls

--- sample_apps/src/main/java/controller/AppController.java
+++ sample_apps/src/main/java/controller/AppController.java
@@ -23,43 +23,35 @@ public class AppController {
     @Autowired
     private AppMonitor monitor;
 
-//    @Autowired
-//    private SampleAppR app1;
-//
-//    @GetMapping("/app1")
-//    @ResponseBody
-//    public ResponseEntity<String> executeApp1(@RequestParam(name = "message") String message, @RequestParam(name = "number") String number) {
-//
-//        int value;
-//        try {
-//            value = Integer.parseInt(number);
-//
-//        } catch (Exception ex) {
-//            return ResponseEntity.badRequest().body(null);
-//        }
-//
-////        try {
-//        return app1.execute(new String[]{"number", "message"}, new String[]{String.valueOf(value), message});
-//
-////        } catch (IOException e) {
-////            logger.error(e.getMessage());
-////            return ResponseEntity.badRequest().body(e.getMessage());
-////        } catch (URISyntaxException e) {
-////            logger.error(e.getMessage());
-////            return ResponseEntity.badRequest().body(e.getMessage());
-////        }
-//
-//    }
+    @Autowired
+    private SampleApp1 app1;
 
-//    @Autowired
-//    private SampleApp2R echo;
-//
-//    @GetMapping("/echo")
-//    @ResponseBody
-//    public ResponseEntity<String> executeApp2(@RequestParam(name = "message") String message) {
-//
-//        return echo.execute(new String[]{"message"}, new String[]{message});
-//    }
+    @GetMapping("/app1")
+    @ResponseBody
+    public ResponseEntity<String> executeApp1(@RequestParam(name = "message") String message, @RequestParam(name = "number") String number) {
+
+        int value;
+        try {
+            value = Integer.parseInt(number);
+
+        } catch (Exception ex) {
+            return ResponseEntity.badRequest().body(null);
+        }
+
+        return app1.execute(new String[]{"number", "message"}, new String[]{String.valueOf(value), message});
+
+    }
+
+    @Autowired
+    private EchoApp echo;
+
+    @GetMapping("/echo")
+    @ResponseBody
+    public ResponseEntity<String> executeEchoApp(@RequestParam(name = "message") String message) {
+
+        return echo.execute(new String[]{"message"}, new String[]{message});
+    }
+
 
     @Autowired
     private SortApp sort;
@@ -79,44 +71,44 @@ public class AppController {
         return sort.execute(new String[]{"size"}, new String[]{String.valueOf(value)});
     }
 
-//    @Autowired
-//    private PrimeApp prime;
-//
-//    @GetMapping("/prime")
-//    @ResponseBody
-//    public ResponseEntity<String> executePrimeApp(@RequestParam(name = "number") String number) {
-//
-//        int value;
-//        try {
-//            value = Integer.parseInt(number);
-//
-//        } catch (Exception ex) {
-//            return ResponseEntity.badRequest().body(null);
-//        }
-//
-//        return prime.execute(new String[]{"number"}, new String[]{String.valueOf(value)});
-//    }
+    @Autowired
+    private PrimeApp prime;
+
+    @GetMapping("/prime")
+    @ResponseBody
+    public ResponseEntity<String> executePrimeApp(@RequestParam(name = "number") String number) {
+
+        int value;
+        try {
+            value = Integer.parseInt(number);
+
+        } catch (Exception ex) {
+            return ResponseEntity.badRequest().body(null);
+        }
+
+        return prime.execute(new String[]{"number"}, new String[]{String.valueOf(value)});
+    }
 
 
-//    @Autowired
-//    private SampleApp2 app2;
-//
-//    @GetMapping("/app2")
-//    @ResponseBody
-//    public ResponseEntity<String> executeApp2(@RequestParam(name = "size") String size, @RequestParam(name = "number") String number) {
-//
-//        int value_number, value_size;
-//        try {
-//            value_number = Integer.parseInt(number);
-//            value_size = Integer.parseInt(size);
-//
-//        } catch (Exception ex) {
-//            return ResponseEntity.badRequest().body("size or number is not numeric");
-//        }
-//
-//        return app2.execute(new String[]{"number", "size"}, new String[]{String.valueOf(value_number), String.valueOf(value_size)});
-//
-//    }
+    @Autowired
+    private SampleApp2 app2;
+
+    @GetMapping("/app2")
+    @ResponseBody
+    public ResponseEntity<String> executeApp2(@RequestParam(name = "size") String size, @RequestParam(name = "number") String number) {
+
+        int value_number, value_size;
+        try {
+            value_number = Integer.parseInt(number);
+            value_size = Integer.parseInt(size);
+
+        } catch (Exception ex) {
+            return ResponseEntity.badRequest().body("size or number is not numeric");
+        }
+
+        return app2.execute(new String[]{"number", "size"}, new String[]{String.valueOf(value_number), String.valueOf(value_size)});
+
+    }
 
 
     @GetMapping("/get_stats")
