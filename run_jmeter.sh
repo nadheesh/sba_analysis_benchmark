e02fa8cb5636e1f67e7b53e723936a054451450e
nadheesh <nadheeshj@gmail.com>
2020-01-31 09:07:23 +0530

    update modules

--- run_jmeter.sh
+++ run_jmeter.sh
@@ -2,12 +2,15 @@
 
 echo process id : $$
 
+curl http://192.168.32.12:8090/pop_stats?filename=temp
+sleep 5
+
 if [[ $# == 2 ]]
 then
     volume_path=$1
     sample_app=$2
 else
-    echo provide .jmx path
+    echo provide mount directory and .jmx path
 fi
 
 # exports
@@ -16,28 +19,35 @@ export volume_path=$(realpath ${volume_path}) && \
 export jmeter_path=/mnt/jmeter && \
 
 #.jmx files
-sample_app=${sample_app:=app1}
+#sample_app=${sample_app:=app1}
 
 # user variables
-arr_no_of_users=(1 10 100 500)
-arr_message_size=(100 1000 5000)
-arr_prime_size=(100000 1000000 10000000)
+arr_no_of_users=(100 200)
+arr_message_size=(200 2000 4000)
+arr_prime=(35171 1299827)
+
+#arr_no_of_users=(100)
+#arr_message_size=(200)
+#arr_prime=(1299827)
+
+run_time=200
 
 # file paths
-log_dir=${jmeter_path}/client/${sample_app}_${timestamp}
+log_dir=${jmeter_path}/jtls/${sample_app}_${timestamp}
 
-mkdir log_dir
+#mkdir ${log_dir}
 
+# shellcheck disable=SC2068
 for no_of_users in ${arr_no_of_users[@]} ; do
     for message_size in ${arr_message_size[@]}; do
-        for prime_size in ${arr_prime_size[@]} ; do
+        for prime_number in ${arr_prime[@]} ; do
             echo
             echo running performance test with \
                 no. of users: ${no_of_users}, \
                 message size: ${message_size}, \
-                prime size: ${prime_size}
+                prime number: ${prime_number}
 
-            file_name=u${no_of_users}_m${message_size}_p${prime_size}
+            file_name=u${no_of_users}_m${message_size}_p${prime_number}_${sample_app}
             summary_path=${log_dir}/${file_name}.csv
 
             sudo docker run --cpus 2 \
@@ -45,18 +55,23 @@ for no_of_users in ${arr_no_of_users[@]} ; do
               vmarrazzo/jmeter \
               -JnoOfThreads=${no_of_users} \
               -JmessageSize=${message_size} \
-              -JnumberSize=${prime_size} \
-              -JresultPath=${summary_path} \
+              -JprimeNumber=${prime_number} \
+              -JrunTime=${run_time} \
+              -JthinkTime=50 \
               -n -X \
               -t ${jmeter_path}/${sample_app}.jmx \
               -l ${log_dir}/${file_name}.jtl \
               -j ${log_dir}/${file_name}.log
 
-             sleep 3
+             sleep 5
 
              echo saving stats collected within app:${sample_app}
              echo calling http://192.168.32.12:8090/pop_stats?filename=${file_name}_${timestamp}
              curl http://192.168.32.12:8090/pop_stats?filename=${file_name}_${timestamp}
+
+             sudo docker container prune -f
+
+             sleep 25
         done
     done
 done
